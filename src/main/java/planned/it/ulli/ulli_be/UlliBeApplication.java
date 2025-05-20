21070ddefc1e4a0148552bb786d82e5a876e3186
hmkim199 <hmkim199@gmail.com>
2025-05-20 22:45:16 +0900

    chore: 프로젝트 초기 환경 설정

---
+++ src/main/java/planned/it/ulli/ulli_be/UlliBeApplication.java
@@ -0,0 +1,13 @@
+package planned.it.ulli.ulli_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlliBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UlliBeApplication.class, args);
+	}
+
+}
